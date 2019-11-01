33863ceb6ab8ad1958ed1bd31c9b2f0fb603ba37
Wes <wesley.qb.wen@gmail.com>
2019-11-01 14:02:03 -0700

    Fixed formatting issue

--- database/controller.js
+++ database/controller.js
@@ -1,28 +1,28 @@
-const { db, Weather } = require('../database/index.js');
+const { Weather } = require('../database/index.js');
 
 var findWeatherData = (callback) => {
-    Weather.find({}, (err, docs) => {
-        if (err) {
-            return callback(err, null);
-        } else if (!docs) {
-            return callback(null, docs);
-        } else {
-            return callback(null, docs);
-        }
-    })
+  Weather.find({}, (err, docs) => {
+    if (err) {
+        return callback(err, null);
+    } else if (!docs) {
+        return callback(null, docs);
+    } else {
+        return callback(null, docs);
+    }
+  })
 }
 
 var insertWeatherData = (weatherData) => {
-    for (var i = 0; i < weatherData.length; i++) {
-        var weatherObj = new Weather({
-            date: weatherData[i].dt_txt,
-            main: weatherData[i].main,
-            weather: weatherData[i].weather[0]
-        });
-        weatherObj.save((err, weatherObj) => {
-            if (err) return console.log(err)
-        })
-    }
+  for (var i = 0; i < weatherData.length; i++) {
+    var weatherObj = new Weather({
+      date: weatherData[i].dt_txt,
+      main: weatherData[i].main,
+      weather: weatherData[i].weather[0]
+    });
+    weatherObj.save((err, weatherObj) => {
+      if (err) return console.log(err)
+    })
+  }
 }
 
 module.exports = {
